d114bbe618527664e12f92378cf536e571eb5804
vickyjayswal12 <vickyjayswal12@gmail.com>
2024-02-26 19:02:41 +0530

    how to use object inplace of map

---
+++ object_As_Map.js
@@ -0,0 +1,17 @@
+// obj.hasOwnProperty(prop) as map.has(key)
+
+const developer = {
+    name: "Jessica Wilkins",
+    country: "United States",
+    isEmployed: true
+  };
+  developer.hasOwnProperty("isEmployed")
+
+  //for set in obj
+
+  developer['age']=12; //set as in map
+
+  //for  get in obj
+   developer[name]
+//    or
+developer.name
